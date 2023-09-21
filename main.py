912e1e8bea2a9c9a485210f99dc721c1a9b34bfa
Ovodov Andrey <oai@centin.ru>
2023-09-21 13:13:26 +0300

    work on raspberry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+import os
 import creds
+
 import telebot
 from telebot import async_telebot
 import asyncio
@@ -7,35 +9,35 @@ import asyncio
 bot = async_telebot.AsyncTeleBot(creds.api_token)
 
 async def add_line_to_csv(message):
-    print ("there")
-    with open (f"{datetime.now().year}_{datetime.now().date().strftime('%m')}.csv", 'a+', encoding="utf-8") as file:
-        print("here")
-        date = datetime.now().date().strftime("%Y-%m-%d")
-        print (date)
-        time = datetime.now().time().strftime("%H:%M:%S")
-        print (time)
-        name = message.from_user.first_name
-        print (name)
-        if  len(message.text.split()) > 2:
-            category, amount, describtion = message.text.split(" ", 2)
-        elif len(message.text.split()) == 2:
-            category, amount= message.text.split()
-            describtion = " "
-        else:
-            await bot.send_message(message.chat.id, "Error in parsing. Line not saved")
-            return 0
-        print("save data")
-        print(f"{date};{time};{name};{category};{amount};{describtion}\n")
-        try:     
-            file.write(f"{date};{time};{name};{category};{amount};{describtion}\n")
-            await bot.send_message(message.chat.id, f"Сохранено {amount}руб в категории {category}")
+    try:
+        with open (f"{os.path.dirname(__file__)}/{datetime.now().year}_{datetime.now().date().strftime('%m')}.csv", 'a+', encoding="utf-8") as file:
+            date = datetime.now().date().strftime("%Y-%m-%d")
+            #print (date)
+            time = datetime.now().time().strftime("%H:%M:%S")
+            #print (time)
+            name = message.from_user.first_name
+            #print (name)
+            if  len(message.text.split()) > 2:
+                category, amount, describtion = message.text.split(" ", 2)
+            elif len(message.text.split()) == 2:
+                category, amount= message.text.split()
+                describtion = " "
+            else:
+                await bot.send_message(message.chat.id, "Error in parsing. Line not saved")
+                return 0
+            #print("save data")
             print(f"{date};{time};{name};{category};{amount};{describtion}\n")
-            return 1
-        except:
-            await bot.send_message(message.chat.id, "Error. Line not saved")
-            
-        print (f"recive message from {message.from_user.id}\n")    
-
+            try:     
+                file.write(f"{date};{time};{name};{category};{amount};{describtion}\n")
+                await bot.send_message(message.chat.id, f"Сохранено {amount}руб в категории {category}") # в файл {file.name}")
+                print(f"{date};{time};{name};{category};{amount};{describtion}\n")
+                return 1
+            except Exception as error:
+                await bot.send_message(message.chat.id, f"Error. Line not saved: {error}")
+                
+            print (f"recive message from {message.from_user.id}\n")    
+    except Exception as error:
+        await bot.send_message(message.chat.id, f"Error. File not opened: {error}")
 
 @bot.message_handler(content_types=['text'])
 async def get_text_messages(message):
@@ -49,7 +51,7 @@ async def get_text_messages(message):
 
 if __name__ == '__main__':
     print("start bot")  
-    asyncio.run(bot.polling())
+    asyncio.run(bot.polling(non_stop=True, request_timeout=90))
 
 #bot.polling(none_stop=True, interval=0) #обязательная для работы бота часть
 
